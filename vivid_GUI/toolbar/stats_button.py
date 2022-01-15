549e9f0b6fe4f8ac187496c5e7a47cc991009206
Siona Vivian <SionaSVivian@outlook.com>
2022-01-15 13:08:44 -0800

    ImageController: modify logic, add documentation

--- vivid_GUI/toolbar/stats_button.py
+++ vivid_GUI/toolbar/stats_button.py
@@ -41,7 +41,7 @@ class StatsButton(ToolbarButton):
          ]))
     container.add_widget(
       self.generate_modal_row(
-        [Button(text="Recheck images", on_press=self.existance_check)]))
+        [Button(text="Recheck images", on_press=self.existence_check)]))
     return container
 
   def generate_modal_row(self, widgets):
@@ -56,8 +56,8 @@ class StatsButton(ToolbarButton):
     wrapper.add_widget(Widget())
     return wrapper
 
-  def existance_check(self, *args):
-    ImageController().existance_check()
+  def existence_check(self, *args):
+    ImageController().existence_check()
     Store.select(lambda state: state['refresh'])()
     self.show_modal()
 

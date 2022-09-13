1f73a69eec91d4561effc220de4887e6071ef644
Siona Vivian <SVivn@proton.me>
2022-09-12 18:33:19 -0700

    vivid: create VividLogger class, VividLogger tests

---
+++ tests/test_vivid_logger.py
@@ -0,0 +1,42 @@
+from vivid.vivid_logger import VividLogger as Logger
+
+
+class TestVividLogger:
+    test_messages = ["This is a message", "SEVERE WARNING", "GENERIC ERROR"]
+
+    def test_register(self):
+        logger_a = Logger("test")
+        logger_a.register("msg_a", self.test_messages[0])
+        assert callable(logger_a.msg_a) == True
+        assert hasattr(Logger("throw_away"), "msg_a") == False
+        assert callable(Logger("test").msg_a) == True
+
+    def test_write(self, capfd):
+        Logger.set_print_mode("console")
+        logger = Logger("test")
+        logger.register("msg_b", self.test_messages[1], level=4)
+        logger.register("msg_c", self.test_messages[2], level=3)
+
+        logger.msg_a()
+        out, err = capfd.readouterr()
+        assert out == f"[\033[0;33m WARNING \033[0;0m] {self.test_messages[0]}\n"
+
+        logger.msg_b()
+        out, err = capfd.readouterr()
+        assert out == f"[\033[1;31m CRITICAL \033[0;0m] {self.test_messages[1]}\n"
+
+        logger.msg_c()
+        out, err = capfd.readouterr()
+        assert out == f"[\033[0;31m ERROR \033[0;0m] {self.test_messages[2]}\n"
+
+    def test_print_history(self, capfd):
+        Logger("test").print_history()
+        out, err = capfd.readouterr()
+        out = out.split("\n")
+        msgs = [
+            f"[\033[0;33m WARNING \033[0;0m] {self.test_messages[0]}",
+            f"[\033[1;31m CRITICAL \033[0;0m] {self.test_messages[1]}",
+            f"[\033[0;31m ERROR \033[0;0m] {self.test_messages[2]}",
+        ]
+        for i in range(0, 3):
+            assert out[i].split("|")[1] == " " + msgs[i]
---
+++ vivid/vivid_logger.py
@@ -0,0 +1,63 @@
+from datetime import datetime
+
+
+class VividLogger:
+    dt = datetime
+    # { name: {} }
+    tags = {}
+    # { tag_name: [ [timestamp, message] ]}
+    history = {}
+    print_mode = "history"
+    TEXT_STYLE_COLOR = ["0;37", "0;32", "0;33", "0;31", "1;31"]
+    LEVELS = ["debug", "info", "warning", "error", "critical"]
+
+    def __init__(self, tag):
+        self._set_tag(tag)
+        self.refresh_all()
+
+    def register(self, name, message, **kwargs):
+        level = kwargs.get("level", 2)
+
+        VividLogger.tags[self.tag][name] = {
+            "message": message,
+            "level": level,
+        }
+
+        self.refresh(name)
+
+    def refresh_all(self):
+        for name in VividLogger.tags[self.tag]:
+            self.refresh(name)
+
+    def refresh(self, name):
+        data = VividLogger.tags[self.tag][name]
+        data["name"] = name
+
+        setattr(self, name, lambda *args: self.write(message_data=data, *args))
+
+    def write(self, message_data, *text_args):
+        level = message_data["level"]
+
+        message = "[\033[%sm %s \033[0;0m] %s" % (
+            VividLogger.TEXT_STYLE_COLOR[level],
+            VividLogger.LEVELS[level].upper(),
+            message_data["message"] % text_args,
+        )
+        time = self.dt.now().strftime("%Y-%m-%d %H:%M:%S")
+        VividLogger.history[self.tag].append([time, message])
+        if VividLogger.print_mode == "console":
+            print(message)
+
+    def print_history(self):
+        for message_data in VividLogger.history[self.tag]:
+            print(f"{message_data[0]}| {message_data[1]}")
+
+    @classmethod
+    def set_print_mode(cls, mode):
+        VividLogger.print_mode = mode
+
+    def _set_tag(self, tag):
+        if tag not in VividLogger.tags:
+            VividLogger.tags[tag] = {}
+            VividLogger.history[tag] = []
+        self.tag = tag
